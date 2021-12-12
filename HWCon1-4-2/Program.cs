80582a3406401ad9c60ff0d97731c12bc4ed6292
GenZProg <GenZ.prog@outlook.com>
2021-12-12 12:28:49 +0800

    Добавьте файлы проекта.

---
+++ HWCon1-4-2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWCon1_4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Hello");
+            Write(" World");
+            Write(" !!!");
+            ReadLine();
+        }
+    }
+}
